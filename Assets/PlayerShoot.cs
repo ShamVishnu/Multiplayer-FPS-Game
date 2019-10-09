07959b89a964da4ee2fb36941782c27b64118d1b
ShamVishnu K <shamvishnu16@gmail.com>
2019-10-09 12:26:21 +0530

    Without Shooting Graphics and Damage

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerShoot : NetworkBehaviour
+{
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("Player Shoot : No Camera Referenced");
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            RaycastHit _hit;
+            if (Physics.Raycast(cam.transform.position,cam.transform.forward,out _hit,weapon.range,mask))
+            {
+                //We hit Something
+                Debug.Log("We Hit " + _hit.collider.name);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera cam;
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private float cameraRotationX = 0f;
+    private float currentCameraRotationX = 0f;
+    private Vector3 thrusterForce = Vector3.zero;
+
+    [SerializeField]
+    private float cameraRotationLimit = 85f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Gets a Movement Vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Gets a Camera Rotational Vector
+    public void RotateCamera(float _cameraRotationX)
+    {
+        cameraRotationX = _cameraRotationX;
+    }
+
+    //Get a force for our thruster
+    public void ApplyThruster(Vector3 _thrusterForce)
+    {
+        thrusterForce = _thrusterForce;
+    }
+
+    //Gets a Rotational Vector
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    //Run Every physics iterations
+    void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Perform Movement based on the velocity variable
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+        if (thrusterForce != Vector3.zero)
+        {
+            rb.AddForce(thrusterForce * Time.fixedDeltaTime,ForceMode.Acceleration);
+        }
+    }
+
+    //Perform Rotation based on the velocity variable
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        if (cam != null)
+        {
+            //  Old Rotational Calculation : cam.transform.Rotate(-cameraRotation);
+            // New Rotational Calculation
+            currentCameraRotationX -= cameraRotationX;
+            currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+
+            cam.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+        }
+
+    }
+}
+
